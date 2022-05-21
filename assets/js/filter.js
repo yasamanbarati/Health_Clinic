026272c4c9de8ef8473c1ac34e92c322f66f40d0
yasaman <yasamanbarati81@gmail.com>
2022-05-21 15:28:20 +0430

    #fix Test_step03.html

--- assets/js/filter.js
+++ assets/js/filter.js
@@ -1,6 +1,15 @@
 function filterFunction() {
     var input, filter, ul, li, a, i;
     input = document.getElementById("dropdownMenuButton1");
+    let inputText = document.getElementById("dropdownMenuButton1").value;
+
+    // const done_btn = document.createElement("button");
+    // done_btn.classList.add("filter_close");
+    // const done_i = document.createElement("i");
+    // done_i.classList.add("material-icons");
+    // done_btn.appendChild(done_i);
+    // inputText.appendChild(done_btn);
+
     filter = input.value.toUpperCase();
     div = document.getElementById("myDropdown");
     a = div.getElementsByTagName("a");
@@ -12,4 +21,17 @@ function filterFunction() {
             a[i].style.display = "none";
         }
     }
+}
+function createSpan() {
+    let inputText = document.getElementById("dropdownMenuButton1").value;
+    // create dic value
+    let dicBox_span = document.createElement("section");
+    dicBox_span.classList.add("dis_box_span");
+    let dicBox_btn = document.createElement("button");
+    dicBox_btn.classList.add("dis_box_btn");
+    let dicBox_i = document.createElement("i");
+    dicBox_i.classList.add("material-icons");
+    dicBox_btn.appendChild(dicBox_i);
+    dicBox_span.appendChild(dicBox_btn);
+    
 }
